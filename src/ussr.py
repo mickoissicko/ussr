dc892f663409ba285a554ae2a298ab52d17dfd30
mickey <139474837+mickoissicko@users.noreply.github.com>
2023-12-29 17:52:24 +0100

    Update ussr.py

--- src/ussr.py
+++ src/ussr.py
@@ -17,7 +17,14 @@ def execute_script(script_name):
     
     print(f"Executing {script_name} script. Path: {script_path}")
 
-    subprocess.Popen([script_path], shell=True, creationflags=subprocess.CREATE_NEW_CONSOLE)
+    process = subprocess.Popen([script_path], shell=True, creationflags=subprocess.CREATE_NEW_CONSOLE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+
+    print(f"Script output:\n{stdout.decode('utf-8')}")
+    print(f"Script error:\n{stderr.decode('utf-8')}")
+
+    if process.returncode != 0:
+        raise Exception(f"Script execution failed with return code {process.returncode}")
 
 @app.route('/start')
 def start():
@@ -26,8 +33,21 @@ def start():
 
 @app.route('/stop')
 def stop():
-    execute_script('stop')
-    return "Script stopped successfully!"
+    try:
+        execute_script('stop')
+        return "Script stopped successfully!"
+    except Exception as e:
+        print(f"Error stopping script: {e}")
+        return "Error stopping script!"
+
+@app.route('/stop-on-website')
+def stop_on_website():
+    try:
+        execute_script('stop')
+        return "Stop script executed successfully!"
+    except Exception as e:
+        print(f"Error stopping script on website: {e}")
+        return "Error stopping script on website!"
 
 if __name__ == '__main__':
     app.run(debug=True)
