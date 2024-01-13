65655d50eb8e3b0c35dd2755693a2d25532aac98
mickey <139474837+mickoissicko@users.noreply.github.com>
2024-01-13 14:03:40 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo pacman -Sy python python-pip
+
+cd server/
+git clone https://aur.archlinux.org/python-flask-git.git
+cd python-flask-git
+makepkg -si
+
+cd script/
+git clone https://aur.archlinux.org/mcrcon.git
+cd mcrcon
+makepkg -si
+
+cd ..
+sudo python ussr-ssl.py
