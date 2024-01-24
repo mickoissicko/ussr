ae78059c6ed21d6d25d14dbc457bec7034770583
mickoissicko <defualtusermick@gmail.com>
2024-01-24 11:22:04 +0100

    fixed only sending some urls fixed scripts executing too fast fixed scripts executing too fast fixed only sending some urls

--- archlinux/server-arch/script/starter.sh
+++ archlinux/server-arch/script/starter.sh
@@ -1,8 +1,13 @@
 # !/bin/bash
 # starter.sh
 
+sleep 1 &
 ngrok tcp --region ap 25565 &
+sleep 1 &
 ngrok tcp --region us 25565 &
+sleep 1 &
 ngrok tcp --region au 25565 &
+sleep 1 &
 ngrok tcp --region eu 25565 &
+sleep 1 &
 ngrok tcp --region in 25565
--- windows/server-windows/script/starter.bat
+++ windows/server-windows/script/starter.bat
@@ -1,9 +1,14 @@
 @echo off
 
+timeout /t 1 /nobreak
 start /b ngrok tcp --region ap 25565
+timeout /t 1 /nobreak
 start /b ngrok tcp --region us 25565
+timeout /t 1 /nobreak
 start /b ngrok tcp --region au 25565
+timeout /t 1 /nobreak
 start /b ngrok tcp --region eu 25565
+timeout /t 1 /nobreak
 start /b ngrok tcp --region in 25565
 
 timeout /t 15 /nobreak
