962a9fb7ef785e5ae676987c1695a9abb31982aa
mickoissicko <defualtusermick@gmail.com>
2024-01-26 09:46:35 +0100

    fixed flask not installed

--- archlinux/install.sh
+++ archlinux/install.sh
@@ -2,18 +2,15 @@
 
 sudo pacman -Sy python python-pip
 sudo pacman -S python-requests
+sudo pacman -S python-flask
 sudo pacman -S git
 
 cd tmp/
-git clone https://aur.archlinux.org/python-flask-git.git
-cd python-flask-git
-makepkg -si
-
 git clone https://aur.archlinux.org/ngrok.git
 cd ngrok
 makepkg -si
 
-cd server-arch/script/
+cd ../server-arch/script/
 git clone https://aur.archlinux.org/mcrcon.git
 cd mcrcon
 makepkg -si
